195230b6ca32918f4e9e5ca881b25ae0957a0b5b
ratan <mail@ratankumar.org>
2014-08-03 19:45:26 +0530

    basic bootstap template added

--- mytweets/settings.py
+++ mytweets/settings.py
@@ -62,6 +62,27 @@ DATABASES = {
     }
 }
 
+#static file directory inclusion
+STATICFILES_DIRS = (            
+            os.path.join(
+                os.path.dirname(__file__),
+                'static',
+            ),
+        )
+
+
+TEMPLATE_DIRS = (
+    BASE_DIR + '/templates/'
+)
+
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
